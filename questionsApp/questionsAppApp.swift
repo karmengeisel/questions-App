d9df6ce4e2084714ff15832533d16749b2aa8134
Karmen <karmengeisel@icloud.com>
2023-06-27 08:59:50 -0600

    Initial Commit

---
+++ questionsApp/questionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionsAppApp.swift
+//  questionsApp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct questionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
